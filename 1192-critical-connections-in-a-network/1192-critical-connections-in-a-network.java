2167b7ef1d7a0f3d1f9ed5bd2e5836c4ff9a0285
Vani <vanigupta3036@gmail.com>
2026-09-11 13:20:18 +0530

    Time: 81 ms (76.36%), Space: 254 MB (22.93%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.java
@@ -1,48 +1,43 @@
 class Solution {
-    int timer=0;
+    int timer = 0;
+
     public List<List<Integer>> criticalConnections(int V, List<List<Integer>> connections) {
-        List<List<Integer>>adj=new ArrayList<>();
-        for(int i=0;i<V;i++){
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
             adj.add(new ArrayList<>());
         }
-            for(List<Integer>edge:connections){
-                int u=edge.get(0);
-                int v=edge.get(1);
-                adj.get(u).add(v);
-                adj.get(v).add(u);
-            }
-        
-        int []dis=new int[V];
-        int low[]=new int[V];
-        Arrays.fill(dis,-1);
-        List<List<Integer>>ans=new ArrayList<>();
-        for(int i=0;i<V;i++){
-            if(dis[i]==-1)dfs(i,-1,adj,dis,low,ans);
+        for (List<Integer> edge : connections) {
+            int u = edge.get(0);
+            int v = edge.get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+
+        int[] dis = new int[V];
+        int low[] = new int[V];
+        Arrays.fill(dis, -1);
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            if (dis[i] == -1)
+                dfs(i, -1, adj, dis, low, ans);
         }
         return ans;
     }
-    public void dfs(int node,int parent,List<List<Integer>>adj,int dis[],int []low,List<List<Integer>>ans){
-            dis[node]=low[node]=timer++;
-            for(int v:adj.get(node)){
-                if(v==parent)continue;
-                if(dis[v]==-1){
-                    dfs(v,node,adj,dis,low,ans);
-                    low[node]=Math.min(low[node],low[v]);
-                    if(low[v]>dis[node])
-                        ans.add(Arrays.asList(node,v));
-                }
-                    else
-                        low[node]=Math.min(low[node],dis[v]);
-                
-            }
+
+    public void dfs(int node, int parent, List<List<Integer>> adj, int dis[], int[] low, List<List<Integer>> ans) {
+        dis[node] = low[node] = timer++;
+        for (int v : adj.get(node)) {
+            if (v == parent)
+                continue;
+            if (dis[v] == -1) {
+                dfs(v, node, adj, dis, low, ans);
+                low[node] = Math.min(low[node], low[v]);
+                if (low[v] > dis[node])
+                    ans.add(Arrays.asList(node, v));
+            }//hun ethe matlab element pehla discoverd aa hun check krange ki koi cycle ch back edge tn nhi bn ri kite . 
+            else
+                low[node] = Math.min(low[node], dis[v]);
+
+        }
     }
 }
-
-
-
-
-
-
-
-
-
